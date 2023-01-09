f34ecf289f5e8c13214101f7f40d7b763e235460
Jean Sébastien ORTEGA <jean-sebastien.ortega@laplateforme.io>
2023-01-09 11:34:13 +0100

    Commit Job00, Job01, Job02

---
+++ jour01/job00/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>runtrack2/jour01/job00</title>
+</head>
+<body>
+    <i>Job00</i>
+</body>
+</html>
---
+++ jour01/job01/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>runtrack2/jour01/job01</title>
+</head>
+<body>
+    <i>Job01</i>
+
+<br><br><br>
+
+    <?php  
+         $str="LaPlateforme";
+
+            echo "$str";
+    ?>
+
+<br><br><br>
+
+    <?php  
+         $str="LaPlateforme"; 
+         $str2="Vive";
+         $str3="!"; 
+
+            echo "$str2";
+            echo "$str";
+            echo "$str3";
+    ?>
+
+<br><br><br>
+
+    <?php
+        $val="6";
+
+            echo "$val";
+    ?>
+
+<br><br><br>
+
+    <?php
+
+        echo $val + 4;
+    ?>
+
+<br><br><br>
+
+    <?php
+        $myBool=true;
+            echo "$myBool";
+        $myBool=false;
+            echo "$myBool";
+    ?>
+
+</body>
+</html>
