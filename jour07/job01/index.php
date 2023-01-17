7e1bd35b6724ca5a865f868802bc00c5b1929317
Jean Sébastien ORTEGA <jean-sebastien.ortega@laplateforme.io>
2023-01-17 11:59:08 +0100

    Jour07 (pas fini)

---
+++ jour07/job01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>runtrack2/jour07/job01</title>
+</head>
+<body>
+
+<i>Job01</i>
+<br><br><br>
+
+
+<?php
+
+function hello()
+{
+    echo "Hello LaPlateforme!";
+}
+
+echo hello()
+
+?>
+
+<br><br>
+</form>
+</body>
+</html>
+
---
+++ jour07/job03/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>runtrack2/jour07/job03</title>
+</head>
+<body>
+
+<i>Job03</i>
+<br><br><br>
+
+
+<?php
+
+
+function getHello()
+{
+
+   return "Hello Laplateforme!";
+
+}
+
+echo getHello();
+
+
+
+?>
+
+<br><br>
+</form>
+</body>
+</html>
+
